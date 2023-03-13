6a0b42a469bccc722abaf081d2a610a7a507b199
treasurebenda <115119167+treasurebenda@users.noreply.github.com>
2023-03-13 10:41:32 +0200

    documentation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""pascal's triangle function """
-def pascal_triangle(n)
-""" if n is equal to zero """
+"""
+pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """get pascal triangle to a certain index"""
     if n <= 0:
         return []
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle    
+    result = [[1], [1, 1]]
+    for i in range(2, n):
+        list_above = [0] + result[i - 1] + [0]
+        row = []
+        for j in range(0, len(list_above) - 1):
+            row.append(list_above[j] + list_above[j + 1])
+        result.append(row)
+    return result[:n]
